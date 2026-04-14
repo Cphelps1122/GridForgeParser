432b06d2a6010968f8f3c0a10ae3255e89aafff9
Cphelps1122 <chrisphelps@ymail.com>
2026-04-14 13:37:22 -0500

    Update Google Sheets authentication method
    Refactor authentication to use Streamlit secrets for Google Sheets access.

--- google_sheets.py
+++ google_sheets.py
@@ -1,12 +1,18 @@
 import gspread
+import json
 from google.oauth2.service_account import Credentials
+import streamlit as st
 
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
 def get_sheet(sheet_id: str, worksheet_name: str = None):
-    creds = Credentials.from_service_account_file("service_account.json", scopes=SCOPES)
+    # Load JSON from Streamlit secrets
+    service_account_info = json.loads(st.secrets["google"]["service_account_json"])
+    creds = Credentials.from_service_account_info(service_account_info, scopes=SCOPES)
+
     client = gspread.authorize(creds)
     sh = client.open_by_key(sheet_id)
+
     if worksheet_name:
         return sh.worksheet(worksheet_name)
     return sh.sheet1
